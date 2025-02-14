1d5ee323b72fa025f6bf21cd1f538e9ad3d82afc
chuudino <chuudino@gmail.com>
2025-02-14 15:49:20 +0800

    feat: 新增 utils 模組以初始化頁面和顯示菜單

---
+++ utils/utils.py
@@ -0,0 +1,55 @@
+import os
+import dotenv
+import streamlit as st
+from .menu import menu
+
+dotenv.load_dotenv()
+
+
+def init_page():
+    DEV = os.getenv("dev", "false").lower() == "true"
+    if DEV:
+        pass
+        # import subprocess
+
+        # try:
+        #     subprocess.run(
+        #         ["pipreqs", ".", "--force"], check=True, text=True, capture_output=True
+        #     )
+        # except Exception as e:
+        #     print(f"命令執行失敗：\n{e}")
+
+    st.set_page_config(page_title="CCSC Website", page_icon="⛪", layout="wide")
+    st.logo("static/images/logo.png")
+    menu()
+
+    # 添加版權聲明
+    st.markdown(
+        """
+        <div style="
+            position: fixed;
+            left: 50%;
+            bottom: 20px;
+            transform: translateX(-50%);
+            text-align: center;
+            background-color: transparent;
+            z-index: 1000;
+        ">
+            <p style="margin: 0; font-size: 14px;">Copyright © 中華基督神修小會</p>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+#     st.write(
+#         """
+# <style>
+#     /* 隱藏多餘元素 */
+#     [data-testid="stToolbar"], [data-testid="stDecoration"], [data-testid="stHeader"], footer{
+#         display: none !important;
+#     }
+# </style>
+# """,
+#         unsafe_allow_html=True,
+#     )
